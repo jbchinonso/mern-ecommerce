e2e98f9edd4adbb8d736ca3ec1a8036a0d46b991
johnbosco <nwekejohnbosco@gmail.com>
2021-07-11 01:02:59 +0100

    implement order model and api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter)
 
 app.get('/', (req, res)=>{
     res.send('Server is ready')
